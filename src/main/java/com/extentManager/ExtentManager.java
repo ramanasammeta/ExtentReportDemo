7a192e466126e7a974a4aba8321bff0df70c6d4c
RamanaSametta <ramanasreddy@gmail.com>
2022-01-07 10:53:37 +0000

    Initial commit

---
+++ src/main/java/com/extentManager/ExtentManager.java
@@ -0,0 +1,31 @@
+package com.extentManager;
+
+import com.aventstack.extentreports.*;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
+public class ExtentManager {
+
+    public static ExtentHtmlReporter htmlReporter;
+    public static ExtentReports extent;
+    public static ExtentTest test;
+
+    public static void setExtent() {
+        htmlReporter= new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-output/ExtentReport/"+"MyReport.html");
+        htmlReporter.loadXMLConfig(System.getProperty("user.dir")+"/extent-config.xml");
+        //htmlReporter.config().setDocumentTitle("Automation Test Report");
+        //htmlReporter.config().setReportName("OrangeHRM Test Automation Report");
+        //htmlReporter.config().setTheme(Theme.DARK);
+
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+
+        extent.setSystemInfo("HostName", "MyHost");
+        extent.setSystemInfo("ProjectName", "MyStoreProject");
+        extent.setSystemInfo("Tester", "Ramana");
+        extent.setSystemInfo("OS", "Win10");
+        extent.setSystemInfo("Browser", "Chrome");
+    }
+    public static void endReport() {
+        extent.flush();
+    }
+}
